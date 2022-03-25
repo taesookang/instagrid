1e77d616b1951e240328e03861e7609c6d56914a
Taesoo <tarrak123@gmail.com>
2022-03-25 01:55:46 -0400

    fix: Stored firebase admin sdk secret key as an env variable for deployment

--- firebase/admin/index.ts
+++ firebase/admin/index.ts
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../../secret.json");
+// const serviceAccount = require("../../secret.json");
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 export const verifyIdToken = (token: string) => {
   if (!admin.apps.length) {
     admin.initializeApp({
